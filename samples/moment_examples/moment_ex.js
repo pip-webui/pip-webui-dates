330308817bf01459dc2990db6c2cf070775cd5ad
Anastas Fonotov <anastasf@gmail.com>
2016-10-16 20:06:26 +0300

    locale example

--- samples/moment_examples/moment_ex.js
+++ samples/moment_examples/moment_ex.js
@@ -8,8 +8,21 @@
     thisModule.controller('MomentExController',
         function($scope, $window) {
 
+            $scope.languages = ['en', 'ru'];
+            $scope.language = 'en';
+
+            $scope.onLanguageClick = onLanguageClick; 
+
             $scope.date = new moment();
-            $scope.time_from = moment("20111031", "YYYYMMDD").fromNow();            
+            $scope.time_from = moment("20111031", "YYYYMMDD").fromNow();      
+
+            return;
+
+            function onLanguageClick(language) {
+                $scope.language = language; 
+                moment.locale(language);
+            };
+
         }
     );
 
