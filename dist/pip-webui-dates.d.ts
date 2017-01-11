ba517d5344d13ef25e26c6e4545abe127ddc6ef8
Anastas Fonotov <anastasf@gmail.com>
2017-01-12 01:18:51 +0200

    Updated submodules

--- dist/pip-webui-dates.d.ts
+++ dist/pip-webui-dates.d.ts
@@ -1,6 +1,7 @@
 declare module pip.dates {
 
 
+
 function formatTimeFilter(pipDateTime: any): (value: any, format: string) => string;
 function formatDateOptionalFilter(pipDateTime: any): (value: any, format: string) => string;
 function formatLongDateFilter(pipDateTime: any): (value: any) => string;
@@ -99,7 +100,6 @@ export interface IDateTimeService {
 }
 export interface IDateTimeProvider extends IDateTimeService, ng.IServiceProvider {
 }
-
 
 
 
