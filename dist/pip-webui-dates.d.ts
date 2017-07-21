1a13c8d4686bebec2d92f2ec11edd35763bde570
Anastas Fonotov <anastasf@gmail.com>
2017-07-21 14:41:20 +0300

    'rebuild'

--- dist/pip-webui-dates.d.ts
+++ dist/pip-webui-dates.d.ts
@@ -3,7 +3,6 @@ declare module pip.dates {
 
 
 
-
 export class DateRangeType {
     static Year: string;
     static Month: string;
@@ -76,6 +75,7 @@ export interface IDateFormatService {
 }
 export interface IDateFormatProvider extends IDateFormatService, ng.IServiceProvider {
 }
+
 
 
 
